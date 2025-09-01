a564c906128d34bfa2b0021f5029e8c1d5796b2e
ArcingFirehawk <anthony.choi204@gmail.com>
2025-09-01 18:31:55 +1000

    Added AudioFIle.js class.

---
+++ classes/AudioFile.js
@@ -0,0 +1,30 @@
+/**
+ * @name: AudioFile.js
+ * @description: Class to construct audio objects for based on files.
+ * @author: Anthony Choi.
+ */
+
+
+
+// IMPORTS
+const path = require("path");
+
+
+// CONSTRUCTOR
+class AudioFile {
+    constructor(filePath) {
+        this.filePath = __dirname + "/../" + filePath;
+        this.name = path.basename(filePath);
+    }
+
+
+    // FUNCTIONS
+
+    // Function to print the object's details to console.
+    print()
+    {
+        console.log(`File Name: ${this.name}; File Path: ${this.filePath}.`);
+    }
+}
+
+module.exports = AudioFile;
